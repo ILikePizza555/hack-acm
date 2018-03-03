fc3ce84d638f82a5ae7d6383d71152c296c754df
Isaac Avram <avrisaac555@gmail.com>
2018-03-03 15:54:22 -0800

    Added the ability to join games

--- src/main/scala/com/avrisaac555/hackacm/Data.scala
+++ src/main/scala/com/avrisaac555/hackacm/Data.scala
@@ -3,4 +3,7 @@ package com.avrisaac555.hackacm
 import java.time.LocalDateTime
 
 case class Player(id: Long, name: String)
-case class Game(id: Long, name: String, beginTime: LocalDateTime)
+
+case class Game(id: Long, name: String, beginTime: LocalDateTime, players: List[Player]) {
+    def addPlayer(player: Player) = Game(id, name, beginTime, player :: players)
+}
--- src/main/scala/com/avrisaac555/hackacm/GameServlet.scala
+++ src/main/scala/com/avrisaac555/hackacm/GameServlet.scala
@@ -10,27 +10,30 @@ import scala.collection.mutable
 /**
   * In charge of creating games, displaying it to players, and updating the game state
   */
-class GameServlet(private val games: mutable.MutableList[Game] = mutable.MutableList(),
-                  private val players: mutable.MutableList[Player] = mutable.MutableList()) extends ScalatraServlet
-                                                                                            with ScalateSupport {
-    def getGames: List[Game] = games.toList
+class GameServlet(private val gameMap: mutable.HashMap[Long, Game] = mutable.HashMap()) extends ScalatraServlet with ScalateSupport {
+    def getGames: List[Game] = gameMap.values.toList
     def generateId: Long = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
 
-    def getGameById(id: Long): Option[Game] = games.find(g => g.id == id)
+    def getGameById(id: Long): Option[Game] = gameMap.get(id)
 
     /**
-      * Creates a new game and adds it to the games list
+      * Creates a new game, adds it to the games list, and redirects the player
       */
     post("/") {
         val p = Player(generateId, params.getOrElse("playerName", halt(400, "playerName must exist.")))
-        players += p
 
-        val g = Game(generateId, params.getOrElse("gameName", halt(400, "gameName must exist.")), LocalDateTime.now())
-        games += g
+        val g = Game(generateId,
+                     params.getOrElse("gameName", halt(400, "gameName must exist.")),
+                     LocalDateTime.now(),
+                     p :: Nil)
+        gameMap += g.id -> g
 
         redirect("/game/" + g.id)
     }
 
+    /**
+      * Gets a game
+      */
     get("/:id") {
         val g = getGameById(params("id").toLong).getOrElse(halt(404, "Specified game not found."))
 
@@ -41,7 +44,17 @@ class GameServlet(private val games: mutable.MutableList[Game] = mutable.Mutable
              "header" -> <span>{g.toString}</span>)
     }
 
+    /**
+      * Joins a player to a game
+      */
     get("/:id/join") {
+        val g = getGameById(params("id").toLong).getOrElse(halt(404, "Specified game not found."))
+        val p = Player(generateId, params.getOrElse("playerName", halt(400, "playerName must exist.")))
 
+        if(g.players.lengthCompare(4) >= 0) halt(400, "Specified game is full.")
+
+        gameMap(g.id) = g.addPlayer(p)
+        
+        redirect("/" + g.id)
     }
 }
